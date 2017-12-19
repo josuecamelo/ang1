fe6bf6aa18a2fe5687a8b2a17420bbd56f8ef053
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2017-12-19 09:33:22 -0200

    Entendendo Controllers

--- src/app/hello.js
+++ src/app/hello.js
@@ -3,6 +3,15 @@ angular
   .component('app', {
     templateUrl: 'app/hello.html',
     controller: function () {
-      this.hello = 'Hello World Josué Camelo!';
+      var vm = this //vm view model
+      vm.hello = 'Hello World Josué Camelo!';
+      vm.person = {
+          name: 'Josué',
+          last_name: 'Camelo'
+      };
+
+      vm.alertMe = function(){
+            alert(vm.person.name);
+      };
     }
   });
