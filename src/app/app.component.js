8d6344c0d2d2e1772fbb9fc82ad1a8df15be56c1
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2017-12-19 10:54:28 -0200

    Entendendo Componets - Só está disponivel a partir da versão 1.5

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,3 +1,20 @@
 /**
  * Created by josuecamelo on 19/12/17.
  */
+// @ text
+// = two way databinding
+// < one way databing
+// & function
+
+angular
+    .module('app')
+    .component('hero', {
+        templateUrl: 'app/hero.html',
+        bindings: {
+            hero: '<'
+        },
+        controller: function() {
+            this.hero = 'This is a new component from Angular 1.5 - SON';
+        }
+        //template:
+    });
